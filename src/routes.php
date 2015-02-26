77958cdee217af330118d7358bce44f588e540ce
Azri Jamil <azri@nematix.com>
2015-02-27 01:31:31 +0800

    Remove app specific document from AContent

--- src/routes.php
+++ src/routes.php
@@ -1,11 +1,5 @@
 <?php
 
-#i: Authorized Only
-Route::group(array('before'=>'document.auth'), function(){
-    Route::controller('documents','Atlantis\Document\DocumentController');
-});
-
-
 #i: API
 Route::group(array('prefix'=>'api/v1'), function(){
     ## Messages API
